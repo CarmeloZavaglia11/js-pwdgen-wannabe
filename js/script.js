eb3b24bdfdc9e8a98f25a575c1a660406b521c5b
Carmelo Zavaglia <carmeloz2220@gmail.com>
2020-07-06 13:37:25 +0200

    Complete

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var nome = prompt('il tuo nome?');
+var cognome = prompt('il tuo cognome?');
+var colorePreferito = prompt('il tuo colore preferito?');
+document.getElementById('testo').innerHTML =  "Ecco la tua password:" + " " + nome + cognome + colorePreferito + 19;
